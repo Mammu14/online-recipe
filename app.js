cb1b2bef8b15e5954a79b6ed287f29b62097a121
Chamarthi Mamatha sree <mamathasree1412@gmail.com>
2024-06-24 15:13:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded'), () => {
+    const recipeForm = document.getElementById('recipe-form');
+    const recipeList = document.getElementById('recipes');
+    const searchInput = document.getElementById('search');
+    const recipeDetails = document.getElementById('recipe-details');
+    const recipeTitle = document.getElementById('recipe-title');
+    const recipeIngredients = document.getElementById('recipe-ingredients');
+    const recipeInstructions = document.getElementById('recipe-instructions');
+}
+   
